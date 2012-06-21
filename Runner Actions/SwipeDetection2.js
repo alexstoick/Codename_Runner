0915ce461d5e0d53a0bad0a33c6e72a7be0c985d
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-06-21 16:31:41 +0300

    Changed the way in which swipes are detected.

--- Runner Actions/SwipeDetection2.js
+++ Runner Actions/SwipeDetection2.js
@@ -15,13 +15,88 @@ class SwipeDetection2 extends MonoBehaviour {
 	private var moved:boolean ;
 	private var lastVelocity:float;
 	private var init:boolean = false ;
+	private var analyzedDuringMove:boolean = false ;
+	
+	private var touchPositions:Array = new Array() ;
+	private var timeOfTouch:Array = new Array () ;
+
+	private function touchBegan ( )	
+	{
+		Debug.LogWarning ( "Touch began" ) ;
+		touchPositions.Clear ( ) ;
+		timeOfTouch.Clear ( ) ;
+		touchPositions.push ( touch.position ) ;
+		timeOfTouch.push ( Time.time ) ;
+		analyzedDuringMove = false ;
+	}
+	
+	private function touchMoved ( )
+	{
+		//Debug.Log ( "Touch moved" ) ;
+		var position:Vector2 = touch.position ;
+		var time:double = Time.time ;
+		touchPositions.push ( position ) ;
+		timeOfTouch.push ( time ) ;
+		Debug.Log ( touch.position + " " + Time.time ) ;
+		
+		var firstTouch:Vector2 = touchPositions[0] ;
+		var delta:double = position.x - firstTouch.x ;
+		
+		if ( delta< -50 || delta > 50 )
+		{	
+			if ( delta < 0 )
+				moveRunner.move ( true , false ,true ) ; //left
+			else
+				moveRunner.move ( false , true , true ) ; //right 
+				
+			touchPositions.Clear ( ) ;
+			timeOfTouch.Clear ( ) ;
+			touchPositions.push ( position ) ;
+			timeOfTouch.push ( time ) ;
+			analyzedDuringMove = true ;
+		}
+			
+	}
+	
+	private function touchEnded ( ) 
+	{
+		Debug.LogWarning ( "Touch ended" ) ;
+		if ( touch.tapCount == 1 )
+				moveRunner.fire ( true ) ;
+		if ( ! analyzedDuringMove )
+		{
+			
+			var firstTouch:Vector2 = touchPositions[0] ;
+			var delta:double = touch.position.x - firstTouch.x ;
+			
+			if ( delta < 0 )
+				moveRunner.move ( true , false ,true ) ; //left
+			else
+				if ( delta > 0 )
+					moveRunner.move ( false , true , true ) ; //right 
+		}
+	}
 
 	function Update() 
 	{
 		if ( Input.touchCount == 0 ) 
 			return ;
+			
+		touch = Input.GetTouch ( 0 ) ;
 		
-	    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+		switch ( touch.phase )
+		{
+			case TouchPhase.Began:
+				touchBegan ( ) ; break ;
+			case TouchPhase.Moved:
+				touchMoved ( ) ; break ;
+			case TouchPhase.Ended:
+				touchEnded ( ) ; break ;
+		}
+		
+		
+		
+	/*    if ( Input.GetTouch(0).phase == TouchPhase.Moved) 
 	    {
 			
 	    	touch = Input.GetTouch ( 0 ) ;
@@ -49,6 +124,7 @@ class SwipeDetection2 extends MonoBehaviour {
 			if ( Input.GetTouch(0).tapCount == 1 )
 				moveRunner.fire ( true ) ;
 		}
+		*/
 
 	}
 }
