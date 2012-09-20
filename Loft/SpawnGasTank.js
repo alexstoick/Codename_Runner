e89fa52669f386c0fdf2826a7fccb5f95b2d0585
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-09-20 17:34:33 +0300

    added gas tank spawner.

---
+++ Loft/SpawnGasTank.js
@@ -0,0 +1,42 @@
+#pragma strict
+
+class SpawnGasTank extends MonoBehaviour {
+
+	static var gasTankPool:SpawnPool ;
+	static var prefab:Transform ;
+	
+	var onCooldown:boolean = false ;
+	
+	function Start ( )
+	{
+		if ( ! gasTankPool ) 
+			gasTankPool  = PoolManager.Pools ["GasTank"] ;
+		if ( ! prefab )
+			prefab = gasTankPool.prefabs["gas tank holder"] ;
+	}
+	
+	function Update ( )
+	{
+		if ( ! StartButton.Started ) 
+			return ;
+		if (  LoftMovement.isStopped () )
+			return ;
+
+		if ( ! onCooldown )
+		{
+			Debug.Log ( "spawned GAS tank" + Time.time ) ;
+			var newGasTank = gasTankPool.Spawn ( prefab ) ;
+			var spawn = newGasTank.GetComponent ( SpawnOnLoft ) ;
+			spawn.Init ( ) ;
+			startCooldown ( );
+		}
+	}
+
+	function startCooldown ( )
+	{
+		onCooldown = true ;
+		var extraTime = Mathf.Max ( ( 0.0003 / LoftMovement.movementVariation ) , 1 ) ;
+		yield WaitForSeconds ( 25 * Mathf.Min ( extraTime , 3 ) ) ;
+		onCooldown = false ;
+	}
+}
