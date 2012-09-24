557db79fd68cb80caabe92102ec376ecd6282e25
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-09-24 15:20:15 +0300

    tried another implementation for turrets.

--- Bullet Control/BulletForTurret.js
+++ Bullet Control/BulletForTurret.js
@@ -26,17 +26,53 @@ class BulletForTurret extends MonoBehaviour {
 	{
 		if ( ! transform.gameObject.active )
 			return ;
-		if ( lastTime + 5 < Time.time  )
+		if ( lastTime + 0.5 < Time.time  )
 		{
 			spawnBullets ( ) ;
 			
 		}
 	}	
+	
+	function setRendererFalse ( )
+	{
+		yield WaitForSeconds ( 0.3 ) ;
+		line.renderer.enabled = false ;
+	}
 	function spawnBullets ( )
 	{
 	
 	    var hit : RaycastHit;
-	    var startPosition:Vector3 = Target.position ;
+	    var raycastPosition:Vector3 = Target.position + Vector3 ( Random.Range ( -1 , 1 ) , Random.Range ( -1 , 1 ) , Random.Range ( -1 , 1 ) );
+	    
+   		if ( Physics.Linecast ( transform.position , raycastPosition , hit ) )
+	    {     	
+   			if ( hit.transform.name == "Loft" )
+   				return ;
+			if ( hit.transform.name.Contains ( "plane" ) )
+    		{
+				HealthProgressBar.currHealth -= 2 ;
+				Debug.Log ( "hit by turret:" + HealthProgressBar.currHealth ) ;
+			}
+	  	}
+
+	
+		var startPos:Vector3 = transform.position ;
+		var length = ( raycastPosition - startPos ) ;	
+
+		var point01:Vector3 = startPos + length * Random.Range ( 0.60 , 0.7 ) ;
+		var point02:Vector3 = startPos + length * Random.Range ( 0.75 , 0.9 );	
+
+		line.SetWidth( 0.03 , 0.03 );
+		line.SetVertexCount(2);
+		line.material = aMaterial;
+		
+		line.renderer.enabled = true;
+		line.SetPosition(0, point01);
+		line.SetPosition(1, point02);
+		
+	   	setRendererFalse ( );
+	   	lastTime = Time.time ;
+/*
 	    if ( Physics.Linecast (transform.position, Target.position, hit) )
 		    if ( hit.transform.name != "plane" )
 		    {
@@ -87,5 +123,7 @@ class BulletForTurret extends MonoBehaviour {
 					HealthProgressBar.currHealth -= 3 ;
 					Debug.Log ( HealthProgressBar.currHealth ) ;
 				}
+			*/
 	}
+
 }
