753003414bf90c5096d1e3a92daf2fe5b5951a53
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-09-26 15:59:33 +0300

    removed some old code.

--- Bullet Control/BulletForTurret.js
+++ Bullet Control/BulletForTurret.js
@@ -81,59 +81,5 @@ class BulletForTurret extends MonoBehaviour {
 
 		lastTime = Time.time ;			
 	   	setRendererFalse ( );
-
-/*
-	    if ( Physics.Linecast (transform.position, Target.position, hit) )
-		    if ( hit.transform.name != "plane" )
-		    {
-		    	line.renderer.enabled = false ;
-		    	return ;
-		    }
-	    //Debug.DrawRay ( transform.position , (Target.position-transform.position ), Color.green , 0.5) ;
-	    
-		var startWidth = 0.03;
-		var endWidth = 0.03;
-	
-		var startPos:Vector3 = transform.position ;
-		var length = ( Target.position-transform.position ) ;	
-
-//		Debug.Log ( "shot a bullet from "+ transform.name ) ;
-//		Debug.Log ( startPos + "		" + length ) ;
-		var point01:Vector3 = startPos + length * Random.Range ( 0.6 , 0.8 ) ;
-		var point02:Vector3 = startPos + length ;	
-	
-		line.SetWidth(startWidth, endWidth);
-		line.SetVertexCount(2);
-		line.material = aMaterial;
-		
-		line.renderer.enabled = true;
-		line.SetPosition(0, point01);
-		line.SetPosition(1, point02);
-
-		lastTime = Time.time ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = false ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = true ;
-		yield WaitForSeconds ( 0.2) ;
-		line.renderer.enabled = false ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = true ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = false ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = true ;
-		yield WaitForSeconds ( 0.2 ) ;
-		line.renderer.enabled = false ;
-
-		if ( transform.gameObject.active )
-			if ( Physics.Linecast ( transform.position , Target.position , hit ) )
-				if ( hit.transform.name == "plane" ) 
-				{
-					HealthProgressBar.currHealth -= 3 ;
-					Debug.Log ( HealthProgressBar.currHealth ) ;
-				}
-			*/
 	}
-
 }
