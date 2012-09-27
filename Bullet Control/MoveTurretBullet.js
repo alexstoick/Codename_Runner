2417f2cc42ef3b2bb35e4a5a331e94c25670f391
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-09-27 14:42:18 +0300

    fixed issue with everything going on a curve instead of a straight line.

--- Bullet Control/MoveTurretBullet.js
+++ Bullet Control/MoveTurretBullet.js
@@ -2,15 +2,9 @@
 
 class MoveTurretBullet extends MonoBehaviour {
 
-	static private var player:Transform ;
 	static private var rocksPool: SpawnPool ;
-	private var despawnTime:double ;
-	
-	function Awake ( )
-	{
-		if ( ! player )
-			player = GameObject.Find ( "plane" ).transform ;
-	}
+	private var despawnTime:double = 0.0 ;
+	private var targetLocation:Vector3 ;
 	
 	function Start ( )
 	{
@@ -18,23 +12,25 @@ class MoveTurretBullet extends MonoBehaviour {
 			rocksPool = PoolManager.Pools [ "Rocks" ] ;
 	}
 	
-	function Init ( )
+	function Init ( target:Vector3 )
 	{
-		despawnTime = Time.time + 0.8;
+		targetLocation = target ;
+		despawnTime = Time.time + 0.8 ;
 	}
 	
-	function OnCollisionEnter(CollisionInfo:Collision) 
+	function Update ()
 	{
+		if ( Time.time > despawnTime )
+		{
+			rocksPool. Despawn ( transform ) ;
+			despawnTime = 0.0 ;
+		}
 	
-		var collider:Collider = CollisionInfo.contacts[0].otherCollider ;
-		var child:Transform ;
-		var cname:String = collider.name ;
-
-		if ( cname == "plane" )
-			{
-				return ;
-			}
+		if ( despawnTime )
+		{
+			Debug.Log ( transform.position + "			" +  targetLocation ) ;
+			transform.position = Vector3.Lerp ( transform.position , targetLocation , 5* Time.deltaTime ) ;
+			transform.rotation = Quaternion ( 0 , 0 , 0 , 0 ) ;
+		}
 	}
-
-
 }
