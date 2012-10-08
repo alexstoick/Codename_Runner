9bd9f431e728a22e223b41eb145efd15b13541f0
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-10-08 16:46:53 +0300

    modified some vars.

--- GUI Buttons/ShootRocket.js
+++ GUI Buttons/ShootRocket.js
@@ -3,21 +3,20 @@
 class ShootRocket extends MonoBehaviour {
 	
 	var moveRunner:MoveRunnerNew ;
+	static var width:int ;
+	static var height:int ;
 	
 	function Awake ( )
 	{
 		if ( ! moveRunner )
 			moveRunner = GameObject.Find ( "BigGroup").GetComponent ( "MoveRunnerNew" ) ;
+		width = Screen.width ;
+		height = Screen.height ;
 	}
 	
 	function OnGUI()
 	{
-	
-		var width = Screen.width ;
-		var height = Screen.height ;
-		Debug.Log ( width + "		" + height ) ;
-		
-		if(GUI.Button(Rect(0, height-100 , width/3 , 100 ), "Start"))
+		if(GUI.Button(Rect(0, height-100 , width/3 , 100 ), "SHOOT!"))
 		{
 			moveRunner.fire ( true ) ;
 		}
