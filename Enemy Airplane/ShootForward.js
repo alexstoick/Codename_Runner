3cadf483d0d33d372ef314c8c07e1f1698acb82f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2012-11-06 14:26:13 +0200

    added public vars for ease of editing timers.

--- Enemy Airplane/ShootForward.js
+++ Enemy Airplane/ShootForward.js
@@ -6,7 +6,9 @@ class ShootForward extends MonoBehaviour {
 	private var rockPrefab:Transform ;
 	private var lastTime:double = -10.000 ;
 	private var bulletsInBurst:int = 0 ;
-	private var MAX_bulletsInBurst:int = 8 ;
+	var MAX_bulletsInBurst:int = 8 ;
+	var TIME_BETWEEN_SHOTS:float = 0.1 ;
+	var TIME_BETWEEN_BURSTS:float = 0.5 ;
 
 
 	function Start  ( )
@@ -33,13 +35,13 @@ class ShootForward extends MonoBehaviour {
 		var rockScript : MoveTurretBullet = rock.GetComponent ( MoveTurretBullet ) ;
 		
 		rockScript.Init ( point02 ) ;
-		lastTime = Time.time + 0.1 ;
+		lastTime = Time.time + TIME_BETWEEN_SHOTS ;
 		
 		++bulletsInBurst ;			
 		
 		if ( bulletsInBurst > MAX_bulletsInBurst )
 		{
-			lastTime = Time.time + 0.5 ;
+			lastTime = Time.time + TIME_BETWEEN_BURSTS ;
 			bulletsInBurst = 0 ;
 		}
 	}
