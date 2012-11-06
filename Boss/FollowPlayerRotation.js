2e960499ac198d8df3b112dd8a6b292f43b010cc
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2012-11-06 14:26:00 +0200

    finished class FollowPlayerRotation. added public vars.

--- Boss/FollowPlayerRotation.js
+++ Boss/FollowPlayerRotation.js
@@ -6,13 +6,13 @@ class FollowPlayerRotation extends MonoBehaviour {
 	private var shouldMove:boolean = true ;
 	
 	var freeze:boolean = false ;
-	var target:Quaternion ;
-	private var WAIT_SECONDS_FOR_MOVEMENT:float = 0.50 ;
-
+	var WAIT_SECONDS_FOR_MOVEMENT:float = 2 ;
+	var MOVE_FOR_SECONDS:float = 1.0 ;
+	var LERP_VALUE:float = 1 ; //lower values = slower movement
+	private var tParam:float = 0.0 ;
 	
 	function Init ( )
 	{
-		target = Quaternion ( 0 , 0, 0 , 0 ) ;
 		freeze = false ;
 	}
 	
@@ -27,17 +27,7 @@ class FollowPlayerRotation extends MonoBehaviour {
 			runner = GameObject. Find ( "BigGroup").transform ;
 
 	}
-	
-	private function doubleToInt ( x: double )
-	{
-		var intX : int = System.Math.Floor ( x ) ;
-		
-		if ( x - intX > 0.5 )
-			return intX + 1 ;
-			
-		return intX ;
-	}
-	
+
 	
 	function Update ( )
 	{	
@@ -45,55 +35,29 @@ class FollowPlayerRotation extends MonoBehaviour {
 		if ( ! freeze )
 		{
 			Move ( ) ;
-
-		}
-		
-		if ( ! ( target.x || target.y || target.z || target.w ))
-			return ;
-		if ( transform.rotation == target )
-		{
-			target = Quaternion ( 0 , 0 , 0 , 0 ) ;
 			return ;
 		}
-		transform.localRotation = Quaternion.Slerp( transform.localRotation, target, Time.deltaTime * 4 ); 
-
 	}
-	
-	function Patrol ( ) 
-	{
-	
-		if ( patrolling )
-			return ;
-		
-		if ( computeDirection ( patrolDirection  , "compute for patrolling" , false ) )
-		{
-			patrolling = true ;
-			yield WaitForSeconds ( WAIT_SECONDS_FOR_PATROL ) ;
-			patrolling = false ;
-			return ;
-		}
-		else
-		{
-			if ( patrolDirection  == 1 )
-				patrolDirection  = 2 ;
-			else
-				patrolDirection  = 1 ;
-			
-			patrolling = true ;
-			computeDirection ( patrolDirection  , "compute for patrolling" , false ) ;
-			yield WaitForSeconds ( WAIT_SECONDS_FOR_PATROL ) ;
-			patrolling = false ;
-		}
-		
-	
-	}
-	
+
 	//Currently not using Move
 	function Move ( )
 	{
+		if ( ! shouldMove ) 
+			return ;
+			
 		shouldMove = false ;
 		
 		//rotate towards player rotation
+		tParam += Time.deltaTime ;
+		
+		if ( tParam >= MOVE_FOR_SECONDS )
+		{
+			//now wait
+			yield WaitForSeconds(WAIT_SECONDS_FOR_MOVEMENT) ;
+			tParam = 0 ;
+		}
+		
+		transform.localEulerAngles.z = Mathf.LerpAngle ( transform.localEulerAngles.z , runner.localEulerAngles.z , Time.deltaTime * LERP_VALUE ) ;
 		
 		shouldMove = true ;
 	}
