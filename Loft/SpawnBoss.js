058a33a3305ac1569bfcb1f73cad79c58e465f06
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2012-11-07 15:34:19 +0200

    Modified the camera animation

--- Loft/SpawnBoss.js
+++ Loft/SpawnBoss.js
@@ -4,8 +4,9 @@ class SpawnBoss extends MonoBehaviour {
 
 	static var bossPool:SpawnPool ;
 	static var prefab:Transform ;
-	
+	static private var mainCamera:Camera ;
 	var lastPath:double = 0.0 ;
+	static private var changeCameraFOV:boolean = true ;
 	
 	function Start ( )
 	{
@@ -13,29 +14,26 @@ class SpawnBoss extends MonoBehaviour {
 			bossPool = PoolManager.Pools ["Boss"] ;
 		if ( ! prefab )
 			prefab = bossPool.prefabs["boss"] ;
+		if ( ! mainCamera )
+			mainCamera = GameObject.Find ( "Main Camera" ).GetComponent(Camera) ;
+	}
+	
+	function Spawn ( ) //will be called from global controller when a level is completed
+	{
+		var newBoss = bossPool.Spawn ( prefab ) ;
+		var spawn = newBoss.GetComponent ( SpawnOnLoft ) ;
+		
+		spawn.Init ( ) ;
+		
+		//initiate the camera zoom
+		changeCameraFOV = true ;
 	}
 	
 	function Update ( )
 	{
-		if ( ! StartButton.Started || LoftMovement.isStopped () )
-			return ; 
-
-		if ( LoftMovement.currPath * lastPath >= 0 && LoftMovement.currPath  > lastPath )
+		if ( changeCameraFOV )
 		{
-		
-			lastPath = LoftMovement.currPath + 0.1009 ;
-			
-			if ( lastPath >= 1 )
-			{
-				lastPath -= 1f;
-				lastPath = -1f + lastPath ; 
-			}
-			
-			var newBoss = bossPool.Spawn ( prefab ) ;
-			var spawn = newBoss.GetComponent ( SpawnOnLoft ) ;
-						
-			spawn.Init ( ) ;
-			
+			mainCamera.fov = Mathf.Lerp ( mainCamera.fov , 60 , Time.deltaTime * 0.8 ) ;
 		}
 	}
 }
