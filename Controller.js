d22cfdead8178c3b186ad947cf442b67a9c5754a
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2012-11-07 18:38:25 +0200

    Global controller file added.

---
+++ Controller.js
@@ -0,0 +1,18 @@
+#pragma strict
+
+class Controller extends MonoBehaviour {
+
+	static var showHealthBar:boolean = true ;
+	static var showFuelBar:boolean = true ;
+	static var showFireCooldownBar:boolean = true ;
+	static var showBossHealthBar:boolean = false ;
+	
+	static var bossIsSpawned:boolean = false ;
+	
+	static var weaponDamage:int = 20 ;
+	static var myArmour:int = 30 ;
+	static var TIME_FOR_BOSS:int = 5 ;
+	
+	
+	
+}
